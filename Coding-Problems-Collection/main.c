641b1374f6cc5b5e20b98216f8cf6537ac395075
GoofyAsh6795395 <goofyash6795395jp@outlook.com>
2025-06-27 07:01:53 +0800

    Initial Commit

---
+++ Coding-Problems-Collection/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Coding-Problems-Collection
+//
+//  Created by 朱欣瑞 on 2025/06/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
